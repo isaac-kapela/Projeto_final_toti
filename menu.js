4e0849e6f25dbfcd0ee573b0a4279cd4bc08292d
Carlos Brizuela <carlosbrizuelavargas@gmail.com>
2024-09-01 19:11:08 -0300

    mudancas no codigo

--- menu.js
+++ menu.js
@@ -1,24 +1,21 @@
-import React from "react";
+const http = require('http');
 
-function menulist() {
-const menu = [
-    {nome: 'Carne de Sol', preco: 25.00, descricao: 'arroz, tropero, vinagrete e fritas'};
-    {nome: 'Almôndegas', preco: 22.00, descricao: 'arroz, batata frita, feijão, puré'};
-    {nome: 'Linguiça Toscana', preco: 30.00, descricao: 'arroz, farofa de ovo e couve'};
-    {nome: 'Filê de Tilapia', preco: 25.00, descricao: 'arroz, batata frita, feijão, pure'};
-    {nome: 'Filê grelhado', preco: 35.00, descricao: 'arroz, legumis, feijão'};
-    {nome: 'Bisteca', preco: 25.00, descricao: 'arroz, batata frita, feijão, couve, salada'};
+const server = http.createServer(function (req,res) {
+    const menuList = [
+        {nome: 'Carne de Sol', 'preco': 25.00, descricao: 'arroz, tropero, vinagrete e fritas'},
+        {nome: 'Almôndegas', 'preco': 22.00, descricao: 'arroz, batata frita, feijão, puré'},
+        {nome: 'Linguiça Toscana', 'preco': 30.00, descricao: 'arroz, farofa de ovo e couve'},
+        {nome: 'Filê de Tilapia', 'preco': 25.00, descricao: 'arroz, batata frita, feijão, pure'},
+        {nome: 'Filê grelhado', 'preco': 35.00, descricao: 'arroz, legumis, feijão'},
+        {nome: 'Bisteca', 'preco': 25.00, descricao: 'arroz, batata frita, feijão, couve, salada'},
 
-]
+            //const data = JSON.stringify(menuList);
+                res.JSON(menuList),
+            
+    ]    
+});
 
-const menulist = menu.map(menu => <h1>{menu.nome} {menu.preco} {menu.descricao}</h1>)
+server.listen(5555, function()
+{console.log("ouvindo no port 5555");
 
-return (
-    <div>
-        {menulist}
-    </div>
-)
-
-}
-
-export default menulist;
+})
